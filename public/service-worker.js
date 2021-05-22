800eec6692aff58398961f2e34765530aac9bbf7
Milad <miladalpha1379@gmail.com>
2021-05-23 01:06:58 +0430

    Listen for install event and Listen for activate event

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,11 @@
-console.log('ok')
+self.addEventListener("install" , (e)=>{
+ // Perform some task
+});
+
+self.addEventListener('activate' , (e)=>{
+     // Perform some task
+});
+
+self.addEventListener('fetch',(e)=>{
+    console.log('fetch', e)
+})
