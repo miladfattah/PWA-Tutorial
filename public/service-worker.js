3cf077747a9bf9912e8051e82254023359dc3b0a
Milad <miladalpha1379@gmail.com>
2021-05-25 23:30:09 +0430

    add feature offline mode or cache offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,52 @@
+let CACHE_VERSION = 1.1; // changes version chches
+let CACHE_CURRENT = {
+    static : `front-cache-v${CACHE_VERSION}`
+}
+
 self.addEventListener('install' , (e)=>{
- 
     e.waitUntil(
-        caches.open('front-cashe')
-                .then((cache)=>{
-                    return cache.add("/static/css/materialize.min.css");
-                })
+        caches.open(CACHE_CURRENT['static']).then(cache=>{
+            return cache.addAll(
+                [
+                '/',
+                '/static/css/materialize.min.css',
+                '/static/css/vazir.css',
+                '/static/css/style.css',
+                '/static/js/app.js',
+                '/static/js/materialize.min.js'
+                 ]
+            )
+        })
     )
-
-
 });
 
 self.addEventListener('activate' , (e)=>{
-    console.log('listen to activate event');
+    let expressionVersion = Object.values(CACHE_CURRENT);
+
+    console.log('expre ' , expressionVersion);
+    e.waitUntil(
+        caches.keys().then(cacheNames=>{
+            cacheNames.forEach(cacheName=>{
+                if( ! expressionVersion.includes(cacheName)){
+                    caches.delete(cacheName);
+                }
+            })
+        })
+    )
 });
 
 self.addEventListener('fetch' , (e)=>{
-    e.waitUntil(
-        caches.open('front-cashe')
-                .then(cashe=>{
-                    return cashe.match(e.request).then(response=>{
-                        return response || fetch(e.request);
+    e.respondWith(
+        caches.open('front-cashe').then((cache)=>{
+            return cache.match(e.request).then(response=>{
+                return (
+                    response ||
+                    fetch(e.request).then( (response)=> {
+                      cache.put(e.request, response.clone());
+                      return response;
                     })
-                })
+                  );
+            })
+        })
     )
 })
